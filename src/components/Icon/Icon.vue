94ab01bc99b7d1696d4e4d2c2fbef272ca2a7540
carol cheng <Carol.Cheng@newtype.com.tw>
2024-07-17 15:54:59 +0800

    feat: 引入 icon

---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  iconName: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <component :is="props.iconName" />
+</template>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import ArcoVue from '@arco-design/web-vue';
-import './style.css'
-import App from './App.vue'
-import '@arco-design/web-vue/dist/arco.css';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "@arco-design/web-vue/dist/arco.css";
+import ArcoVueIcon from "@arco-design/web-vue/es/icon";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ArcoVueIcon);
+app.mount("#app");
